f506b41df86543a2cc331652a683b359bb3e5104
João Victor Peixoto de Souza <82658732+JoaoVictor-afk@users.noreply.github.com>
2021-04-22 18:45:09 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+//boolean
+//null
+//undefined
+//number
+//string
+
+//+adição
+//-Subtração
+//*mutiplicação
+// / Divisão
+//% modulo
+//++incremento
+//--decremento
+alert("Hello World");
+
+console.log("ola mundo");
+
+var a = 5;
+
+var b = 20;
+
+++a;
+
+var c = a + b;
+
+a -= b;
+
+console.log(1 + 1);
+
+console.log(a);
