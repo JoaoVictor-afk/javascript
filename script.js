1d73ddcd401f9c7db0ea9da05f6d9ebf14d0c4bf
João Victor Peixoto de Souza <82658732+JoaoVictor-afk@users.noreply.github.com>
2021-04-23 16:16:02 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,22 +1,11 @@
-//boolean
-//null
-//undefined
-//number
-//string
-
-//+adição
-//-Subtração
-//*mutiplicação
-// / Divisão
-//% modulo
-//++incremento
-//--decremento
 alert("Hello World");
 
 console.log("ola mundo");
 
 var c = "5";
+
 var d = 5;
+
 var e = d + parseInt(c);
 
 var idade = 14;
@@ -39,6 +28,12 @@ console.log(e);
 
 var idade = parseFloat(prompt("Digite a sua idade"));
 
+idade >= 18 ? console.log("Pode") : console.log("Não Pode");
+
+var pode = idade >= 18 ? true : false;
+
+console.log(pode);
+
 if (idade >= 19 && idade <= 70) {
 	console.log("pode");
 	console.log("qual o seu pedido?");
@@ -48,4 +43,37 @@ if (idade >= 19 && idade <= 70) {
 } else {
 	console.log("Não pode");
 	console.log("Volte futuramente");
+}
+var nota1 = 6;
+
+var nota2 = 5;
+
+var media = (nota1 + nota2) / 2;
+
+var conceito = "legal";
+
+if (media >= 8.5) {
+	conceito = "bom";
+} else if (media >= 6.5) {
+	conceito = "otimo";
+} else {
+	conceito = "regular";
+}
+console.log(media);
+
+console.log(conceito);
+
+switch (conceito) {
+	case "bom":
+		console.log("Parabens vc esta muito bem");
+		break;
+	case "otimo":
+		console.log("Você esta quase perfeito");
+		break;
+	case "regular":
+		console.log("Estude mais um pouco");
+		break;
+	default:
+		console.log("Houve algum erro");
+		break;
 }
