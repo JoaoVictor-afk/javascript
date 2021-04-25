60442aa9711cc7314be44c49c32dffcd28034a43
João Victor Peixoto de Souza <82658732+JoaoVictor-afk@users.noreply.github.com>
2021-04-25 15:22:15 -0300

    Create alunos.js

---
+++ alunos.js
@@ -0,0 +1,21 @@
+function criarAluno(nome, n1, n2) {
+	return {
+		nome: nome,
+		nota: n1,
+		nota2: n2,
+
+		media: function () {
+			return (this.nota + this.nota2) / 2;
+		},
+	};
+}
+var turma = [
+	criarAluno("Vitor", 9, 1),
+	criarAluno("Luisa", 10, 8),
+	criarAluno("João", 8, 7.5),
+];
+
+turma.forEach(function (turma) {
+	console.log(turma.nome);
+	console.log(turma.media());
+});
