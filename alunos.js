27a12558094bd72c4b5dc4aa10c42a706e76a990
João Victor Peixoto de Souza <82658732+JoaoVictor-afk@users.noreply.github.com>
2021-04-25 21:56:06 -0300

    Update alunos.js

--- alunos.js
+++ alunos.js
@@ -24,4 +24,10 @@ console.log(d);
 console.log(d.getDate());
 console.log(d.getDay());
 
+var numero1 = 4;
+let numero2 = 5;
+const numero3 = 6;
 
+console.log(numero1);
+console.log(numero2);
+console.log(numero3)
